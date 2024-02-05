aaa1d85ef718789f7ab71ff03c7f2591025e1145
Blxssy <roma.asav@gmail.com>
2024-02-05 10:23:17 +0300

    Game on localhost

--- game/cmd/main.go
+++ game/cmd/main.go
@@ -2,33 +2,22 @@ package main
 
 import (
 	"context"
-	"errors"
 	"github.com/blxsyy/gameLife/game/internal/application"
-	"github.com/blxsyy/gameLife/game/internal/config"
-	"log"
 	"os"
 )
 
 func main() {
 	ctx := context.Background()
+	// Exit приводит к завершению программы с заданным кодом.
 	os.Exit(mainWithExitCode(ctx))
 }
 
 func mainWithExitCode(ctx context.Context) int {
-	cfg := config.Config{
-		Width:  10,
-		Height: 10,
+	cfg := application.Config{
+		Width:  20,
+		Height: 20,
 	}
 	app := application.New(cfg)
 
-	if err := app.Run(ctx); err != nil {
-		switch {
-		case errors.Is(err, context.Canceled):
-			log.Println("Processing cancelled")
-		default:
-			log.Println("Application run error", err)
-		}
-		return 1
-	}
-	return 0
+	return app.Run(ctx)
 }
